cf8a771f929109e3fd8f14fe1c315fae4553889d
Jose Antonio <harogjanto@gmail.com>
2023-02-28 17:29:00 -0600

    Java-05

---
+++ Java/Java-05/Control/src/principal/TestMultiple.java
@@ -0,0 +1,17 @@
+package principal;
+
+import control.EstructuraMultiple;
+import control.Ordenar;
+
+public class TestMultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//EstructuraMultiple em = new EstructuraMultiple();
+		//em.vocal();
+		//em.nota();
+		Ordenar or = new Ordenar();
+		or.orden();
+		}
+
+}
