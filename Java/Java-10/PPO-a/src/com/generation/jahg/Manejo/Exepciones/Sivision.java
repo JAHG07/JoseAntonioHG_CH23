9a1da6b57047d80031fec21851dd36933c791cea
Jose Antonio <harogjanto@gmail.com>
2023-03-08 17:07:16 -0600

    Java 11

--- Java/Java-10/PPO-a/src/com/generation/jahg/Manejo/Exepciones/Sivision.java
+++ Java/Java-10/PPO-a/src/com/generation/jahg/Manejo/Exepciones/Sivision.java
@@ -4,11 +4,14 @@ public class Sivision {
 	private int numerador;
 	private int denominador;
 	
-	public Sivision(int numerador, int denominador) {
+	public Sivision(int numerador, int denominador)throws OpExceptions {
 		if (denominador == 0 ) {
 			throw new OpExceptions("El denominador es 0"); 
 		}
 		this.numerador = numerador;
 		this.denominador = denominador;
+	}
+	public void visualozar() {
+		System.out.println("El resultado de la division es :" + (this.numerador/this.denominador));
 	}
 }
--- Java/Java-10/PPO-a/src/com/generation/jahg/Manejo/Exepciones/TestExptions.java
+++ Java/Java-10/PPO-a/src/com/generation/jahg/Manejo/Exepciones/TestExptions.java
@@ -1,5 +1,13 @@
 package com.generation.jahg.Manejo.Exepciones;
 
 public class TestExptions {
-
+	public static void main(String[] args) {
+		try {
+			Sivision d = new Sivision(4, 2);
+		}catch(OpExceptions e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		//System.out.println("Continua la ejecucion");
+	}
 }
